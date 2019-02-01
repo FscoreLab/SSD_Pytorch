3c57c6f796bc2784c77fe28dccb52680ebd9b512
Evgenii Nikitin <en919@nyu.edu>
2019-02-01 15:52:27 -0500

    convert to library

--- data/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from .voc0712 import VOCDetection, detection_collate
-from .coco import *
-from .data_augment import *
-
- 
-
--- layers/functions/__init__.py
+++
@@ -1,6 +0,0 @@
-from .detection import Detect
-from .prior_box import PriorBox
-# from .refine_prior_box import RefinePriorBox
-
-
-__all__ = ['Detect', 'PriorBox']
